871218e7f7885426b3b369791175f566e47219c7
Antonio <antonio.giordano@getapper.com>
2018-11-22 10:56:19 +0100

    Added multi node application business logic

--- tasks/develop.js
+++ tasks/develop.js
@@ -1,74 +1,87 @@
 
-/*!
- *
- * grunt-develop
- * http://github.com/edwardhotchkiss/grunt-develop
- *
- * Copyright (c) 2013 Edward Hotchkiss
- * Licensed under the MIT license.
- *
+/*
+ * @module grunt-multi-develop
+ * @author Antonio Giordano <antonio.giordano@ubiatar.com>
+ * @description http://github.com/ubiatar/grunt-multi-develop/
+ * @license MIT
  */
-
 'use strict';
 
 module.exports = function(grunt) {
-
-  var child
+  var children = {}
+    , running = false
     , fs = require('fs')
     , util = require('util');
-
   // kills child process (server)
-  grunt.event.on('develop.kill', function() {
-    grunt.log.warn('\nrestarting server');
-    child.kill('SIGHUP');
+  grunt.event.on('develop.kill', function(target) {
+    grunt.log.warn('kill process');
+    children[target].kill('SIGKILL');
   });
-
-  // watches server and broadcasts restart on change
-  grunt.event.on('develop.watch', function(filename) {
-    fs.watchFile(filename, { interval: 250 }, function(change) {
-      grunt.log.warn('\nfile changed');
-      grunt.event.emit('develop.kill');
-    });
+  // spawned, notify grunt to move onto next task
+  grunt.event.on('develop.started', function() {
+    setTimeout(function() {
+      global.gruntDevelopDone();
+    }, 250);
   });
-
   // starts server
-  grunt.event.on('develop.start', function(filename) {
-    if (child && !child.killed) {
-      return grunt.event.emit('develop.kill');
+  grunt.event.on('develop.start', function(filename, nodeArgs, args, env, cmd, target) {
+    var spawnArgs = nodeArgs.concat([filename], args);
+    if (children[target]) {
+      return grunt.event.emit('develop.kill', target);
     }
-    child = grunt.util.spawn({
-      cmd: process.argv[0],
-      args: [filename]
-    }, function (error, result, code) {
-      grunt.event.emit('develop.start', filename);
+    children[target] = grunt.util.spawn({
+      cmd: cmd,
+      args: spawnArgs,
+      opts: {
+        env: env
+      }
+    }, function(){});
+    // handle exit
+    children[target].on('exit', function(code, signal) {
+      delete children[target];
+      if (signal !== null) {
+        grunt.log.warn(util.format('application exited with signal %s', signal));
+      } else {
+        grunt.log.warn(util.format('application exited with code %s', code));
+      }
+      if (signal === 'SIGKILL') {
+        grunt.event.emit('develop.start', filename, nodeArgs, args, env, cmd);
+      }
     });
-    grunt.log.ok(util.format('succesfully started server "%s".', filename));
-    child.stdout.setEncoding('utf8');
-    child.stderr.setEncoding('utf8');
-    child.stdout.on('data', function(data) {
-      grunt.log.write(data);
+    children[target].stderr.on('data', function(buffer) {
+      if (buffer.toString().trim().length) {
+        grunt.log.write('\r\n[grunt-develop:' + target + '] > '.red + buffer.toString());
+      }
     });
-    child.stderr.on('data', function(data) {
-      grunt.log.write(data);
+    children[target].stdout.on('data', function (buffer) {
+      grunt.log.write('\r\n[grunt-develop:' + target + '] > '.cyan + buffer.toString());
     });
-    child.on('exit', function(code, signal) {
-      grunt.log.warn(util.format('server exited with: "%s"', signal));
-    });
-    grunt.event.emit('develop.watch', filename);
+    grunt.log.write('\r\n[grunt-develop:' + target + '] > '.cyan + util.format('started application "%s".', filename));
+    grunt.event.emit('develop.started');
   });
-
   // TASK. perform setup
   grunt.registerMultiTask('develop', 'init', function() {
-    var done, filename = this.data.file;
+    var filename = this.data.file
+      , nodeArgs = this.data.nodeArgs || []
+      , args = this.data.args || []
+      , env = this.data.env || process.env || {}
+      , cmd = this.data.cmd || process.argv[0];
     if (!grunt.file.exists(filename)) {
-      grunt.fail.warn(util.format('server file "%s" not found!', filename));
+      grunt.fail.warn(util.format('application file "%s" not found!', filename));
       return false;
     }
-    done = this.async();
-    grunt.event.emit('develop.start', filename);
-    done();
+    global.gruntDevelopDone = this.async();
+    grunt.event.emit('develop.start', filename, nodeArgs, args, env, cmd, this.target);
   });
-
+  process.on('exit', function() {
+    for (let c in children) {
+      try {
+        children[c].kill('SIGINT');
+      } catch (e) {
+        grunt.fail.warn(util.format('Process exit killing target "%s": "%s"!', c, err.message));
+      }
+    }
+  });
 };
 
 /* EOF */
